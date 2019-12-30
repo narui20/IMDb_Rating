9307b3702aa9fa49e25fd233ee4978336bc5fa76
Akash Tiwari <drakashti@gmail.com>
2019-12-30 13:11:01 +0530

    signin

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -1,21 +1,69 @@
 import React, { Component } from "react";
 import Axios from "../axios-handler";
-import "../Profile/Profile.css";
+import "./SignIn.css";
 
 class SignIn extends Component {
+  state = {
+    userId: null,
+    user: [],
+    loading: false
+  };
+
   signInHandler() {
+    const profiles = [];
     Axios.get("/profile.json").then(resp => {
       for (let i in resp.data) {
-        console.log(i);
+        const uid = resp.data[i];
+        console.log(uid[uid]);
+        if (uid[uid] === this.state.userId) {
+          profiles.push({
+            ...resp.data[i],
+            id: i
+          });
+        }
       }
     });
   }
+
+  onChangeHandler = event => {
+    this.setState({ userId: event.target.value });
+  };
+
+  onSignIn = () => {
+    const check = this.state.loading;
+    this.setState({ loading: !check });
+  };
+
   render() {
+    let userDetails = null;
+
+    if (this.state.loading) {
+      console.log("checking.." + this.state.loading);
+
+      userDetails = (
+        <div className="signIn">
+          <label className="label">
+            Enter Your Email Id:
+            <input
+              className="input"
+              type="text"
+              onChange={event => this.onChangeHandler(event)}
+            />
+          </label>
+          <input
+            type="submit"
+            className="button"
+            onClick={this.signInHandler}
+          />
+        </div>
+      );
+    }
     return (
       <div>
-        <button className="button" onClick={this.signInHandler}>
+        <button className="button" onClick={this.onSignIn}>
           SignIn
         </button>
+        {userDetails}
       </div>
     );
   }
